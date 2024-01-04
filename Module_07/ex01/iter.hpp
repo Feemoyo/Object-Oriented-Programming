a2713966e6c85d98ea74435179e95ec1e225af7d
Femoyo <lipemoreira.yorgos@gmail.com>
2024-01-04 19:13:54 -0300

    ajustes module_08 ex01 para ajudar o compilador a se encontrar em qual iter usar e quando

--- Module_07/ex01/iter.hpp
+++ Module_07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: fmoreira <fmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 08:59:38 by fmoreira          #+#    #+#             */
-/*   Updated: 2024/01/04 17:38:22 by fmoreira         ###   ########.fr       */
+/*   Updated: 2024/01/04 18:59:20 by fmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,69 +16,17 @@
 #include <iostream>
 
 template <typename T>
+void	iter(T const *array, size_t len, void (*func)(T const &))
+{
+	for (size_t i = 0; i < len; i++)
+		func(array[i]);
+}
+
+template <typename T>
 void	iter(T *array, size_t len, void (*func)(T &))
 {
 	for (size_t i = 0; i < len; i++)
 		func(array[i]);
-}
-
-template <typename T>
-void	iter(T *array, size_t len, void (*func)(T const &))
-{
-	for (size_t i = 0; i < len; i++)
-		func(array[i]);
-}
-
-template <typename T>
-void	printValue(T &value)
-{
-	std::cout << value << std::endl;
-}
-
-template <typename T>
-void	plusValue(T &value)
-{
-	value += 1;
-}
-
-template <>
-void	plusValue(std::string &value)
-{
-	std::cout << "Not possible with string" << std::endl;
-	(void)value;
-}
-
-template <typename T>
-void	minumValue(T &value)
-{
-	value -= 1;
-}
-
-template <>
-void	minumValue(std::string &value)
-{
-	std::cout << "Not possible with string" << std::endl;
-	(void)value;
-}
-
-template <typename T>
-void	doubleValue(T &value)
-{
-	value *= 2;
-}
-
-template <>
-void	doubleValue(std::string &value)
-{
-	std::cout << "Not possible with string" << std::endl;
-	(void)value;
-}
-
-template <>
-void	doubleValue(char &value)
-{
-	std::cout << "Unfeasible with char" << std::endl;
-	(void)value;
 }
 
 #endif
--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -6,11 +6,63 @@
 /*   By: fmoreira <fmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 08:59:41 by fmoreira          #+#    #+#             */
-/*   Updated: 2023/12/18 13:02:17 by fmoreira         ###   ########.fr       */
+/*   Updated: 2024/01/04 18:59:15 by fmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp" 
+
+template <typename T>
+void	printValue(T &value)
+{
+	std::cout << value << std::endl;
+}
+
+template <typename T>
+void	plusValue(T &value)
+{
+	value += 1;
+}
+
+template <>
+void	plusValue(std::string &value)
+{
+	std::cout << "Not possible with string" << std::endl;
+	(void)value;
+}
+
+template <typename T>
+void	minumValue(T &value)
+{
+	value -= 1;
+}
+
+template <>
+void	minumValue(std::string &value)
+{
+	std::cout << "Not possible with string" << std::endl;
+	(void)value;
+}
+
+template <typename T>
+void	doubleValue(T &value)
+{
+	value *= 2;
+}
+
+template <>
+void	doubleValue(std::string &value)
+{
+	std::cout << "Not possible with string" << std::endl;
+	(void)value;
+}
+
+template <>
+void	doubleValue(char &value)
+{
+	std::cout << "Unfeasible with char" << std::endl;
+	(void)value;
+}
 
 void    intArray(void)
 {
@@ -20,8 +72,8 @@ void    intArray(void)
 	::iter(arrayInt, 5, ::printValue);
 
 	std::cout << "\nintArray * 2" << std::endl;
-	::iter(arrayInt, 5, ::doubleValue);
-	::iter(arrayInt, 5, ::printValue);
+	iter(arrayInt, 5, ::doubleValue);
+	iter(arrayInt, 5, ::printValue);
 
 	std::cout << "\nintArray + 1 and ::printValue" << std::endl;
 	::iter(arrayInt, 5, ::plusValue);
